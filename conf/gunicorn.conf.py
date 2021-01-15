83f0e30c7d48d60e587162922c79e1011403874e
Jonjo McKay <jonjo@jonjomckay.com>
2021-01-15 15:48:05 +0000

    Temporarily commenting out gunicorn log configuration

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -7,8 +7,8 @@ workers = multiprocessing.cpu_count()
 # The maximum number of requests a worker will process before restarting.
 # max_requests = 1000
 
-errorlog = "/var/log/gunicorn/error.log"
-accesslog = "/var/log/gunicorn/access.log"
+# errorlog = "/var/log/gunicorn/error.log"
+# accesslog = "/var/log/gunicorn/access.log"
 loglevel = "info"
 
 timeout = 120
